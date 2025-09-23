f9a16ecf17465266ec61440561283d5270333006
LarissaGloria11 <laladagloriasouza@gmail.com>
2025-09-23 20:29:22 -0300

    Criação Api banco de dados

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import fastify from "fastify"
+import cors from "@fastify/cors"
+
+
+const app = fastify()
+
+async function server(){
+    app.register(cors, {
+        origin: true,
+        methods: ["GER", "POST", "PUT", "PATCH", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"]
+    })
+
+    app.listen({
+        host: '0.0.0.0',
+        port: process.env.PORT
+    }).then(()=> {
+        console.log('HTTP Server is running on PORT:' + process.env.PORT)
+    })
+}
+
+server();
